96a77b62a9cedbe42630b3b0bf9dcf0135279936
Yassine Rhzif <40427561+RHZIF@users.noreply.github.com>
2021-12-04 18:43:05 +0100

    Update task1.py

--- task1.py
+++ task1.py
@@ -42,40 +42,113 @@ def get_image(query):
     return results[1]
 
 
-st.title("Stock data scraper")
-dropdown1 = st.selectbox('Pick your country', py.get_stock_countries())
-dropdown = st.selectbox("Pick your assets", py.get_stocks(country=dropdown1).name)
-start = st.date_input('Start', value =pd.to_datetime('01-01-2000'))
-end = st.date_input('End', value = pd.to_datetime('today'))
-
-start = start.strftime('%d/%m/%Y')
-end = end.strftime('%d/%m/%Y')
-
-stocks = py.get_stocks(country=dropdown1)
-stocks.set_index("name", inplace = True)
-ticker =  stocks.loc[dropdown,'symbol']
-
-df=py.get_stock_historical_data(stock=ticker, country=dropdown1, from_date=start, to_date=end)
-
-url = get_image(str(dropdown))
-print(url)
-url = str(url)
-
-st.image(url, width=300)
-
-st.line_chart(df.Close)
-
-st.dataframe(df)
+st.title("Data scraper")
 
 
-def convert_df(tdf):
-    return df.to_csv().encode('utf-8')
+def stocks():
+    dropdown1 = st.selectbox('Pick your country', py.get_stock_countries())
+    dropdown = st.selectbox("Pick your assets", py.get_stocks(country=dropdown1).name)
+    start = st.date_input('Start', value =pd.to_datetime('01-01-2000'))
+    end = st.date_input('End', value = pd.to_datetime('today'))
 
-csv = convert_df(df)
+    start = start.strftime('%d/%m/%Y')
+    end = end.strftime('%d/%m/%Y')
 
-st.download_button(
-     label="Download data as CSV",
-     data=csv,
-     file_name= str(dropdown) +'.csv',
-     mime='text/csv',
- )
+    stocks = py.get_stocks(country=dropdown1)
+    stocks.set_index("name", inplace = True)
+    ticker =  stocks.loc[dropdown,'symbol']
+
+    df=py.get_stock_historical_data(stock=ticker, country=dropdown1, from_date=start, to_date=end)
+
+    url = get_image(str(dropdown))
+    print(url)
+    url = str(url)
+
+    st.image(url, width=300)
+
+    st.line_chart(df.Close)
+
+    st.dataframe(df)
+
+
+    def convert_df(tdf):
+        return df.to_csv().encode('utf-8')
+
+    csv = convert_df(df)
+
+    st.download_button(
+        label="Download data as CSV",
+        data=csv,
+        file_name= str(dropdown) +'.csv',
+        mime='text/csv',
+    )
+
+def forex():
+    dropdown = st.selectbox("Pick your pair", py.get_currency_crosses_list())
+    start = st.date_input('Start', value =pd.to_datetime('01-01-2000'))
+    end = st.date_input('End', value = pd.to_datetime('today'))
+
+    start = start.strftime('%d/%m/%Y')
+    end = end.strftime('%d/%m/%Y')
+
+    df=py.get_currency_cross_historical_data(currency_cross=dropdown,from_date=start, to_date=end)
+
+    st.line_chart(df.Close)
+
+    st.dataframe(df)
+
+    def convert_df(tdf):
+        return df.to_csv().encode('utf-8')
+
+    csv = convert_df(df)
+
+    st.download_button(
+        label="Download data as CSV",
+        data=csv,
+        file_name= str(dropdown) +'.csv',
+        mime='text/csv',
+    )
+
+def crypto():
+    dropdown = st.selectbox("Pick your asset", py.get_cryptos_list())
+    start = st.date_input('Start', value =pd.to_datetime('01-01-2000'))
+    end = st.date_input('End', value = pd.to_datetime('today'))
+
+    start = start.strftime('%d/%m/%Y')
+    end = end.strftime('%d/%m/%Y')
+
+    df=py.get_crypto_historical_data(crypto=dropdown, from_date=start, to_date=end)
+
+    url = get_image(str(dropdown))
+    print(url)
+    url = str(url)
+
+    st.image(url, width=300)
+
+    st.line_chart(df.Close)
+
+    st.dataframe(df)
+
+
+    def convert_df(tdf):
+        return df.to_csv().encode('utf-8')
+
+    csv = convert_df(df)
+
+    st.download_button(
+        label="Download data as CSV",
+        data=csv,
+        file_name= str(dropdown) +'.csv',
+        mime='text/csv',
+    )
+
+
+
+# Create a page dropdown 
+page = st.selectbox("Choose a market", ["Stocks", "Forex", "Crypto"]) 
+if page == "Stocks":
+    stocks()
+elif page == "Forex":
+    forex()
+elif page == "Crypto":
+    crypto()
