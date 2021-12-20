472bd7972aab37a7dba117c7c9c01d35d1b5aafb
Yassine Rhzif <40427561+RHZIF@users.noreply.github.com>
2021-12-20 20:01:06 +0100

    Update task1.py

--- task1.py
+++ task1.py
@@ -3,7 +3,7 @@ import pandas as pd
 import requests
 import investpy as py
 from bs4 import BeautifulSoup
-import import streamlit as st
+import streamlit as st
 import pandas as pd 
 import requests
 import investpy as py
