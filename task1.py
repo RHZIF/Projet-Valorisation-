d4dadb4f63b1be4b44f5012ef4aab870dc00c682
Yassine Rhzif <40427561+RHZIF@users.noreply.github.com>
2021-12-22 19:05:09 +0100

    Update task1.py

--- task1.py
+++ task1.py
@@ -21,7 +21,7 @@ def get_image(ticker):
 
 apptitle = 'Projet valorisation'
 
-st.set_page_config(page_title=apptitle, page_icon=":eyeglasses:")
+st.set_page_config(page_title=apptitle, page_icon=":stock:")
 
 # Title the app
 st.title('Valorisation des actions cotées en Bourse de Casablanca ')
